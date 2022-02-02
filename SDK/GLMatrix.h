80fd0cd830ee1718548841c19c156951c4e46818
void123456789 <98923611+void123456789@users.noreply.github.com>
2022-02-02 18:55:44 -0500

    Delete SDK directory

--- SDK/GLMatrix.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct GLMatrix {
-public:
-	float matrix[16];
-	float matrix_nest[4][4]; // nested because im compensating :slight_smile: (Just as hard as all the other clientndevelopers ovs)
-};
--- SDK/LevelRender.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class LevelRender {
-public:
-	Vector3 getOrigin() {
-		return *reinterpret_cast<Vector3*>((uintptr_t)this + 0x878);
-	}
-};
